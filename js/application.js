53af5ed3c2d4708d4d59521d9922e0fcb5ade144
Mark Donald Amobi <markdnd007@gmail.com>
2015-07-30 22:05:14 -0500

    undrestand mouse trap and write out mouse trap skeleton

--- js/application.js
+++ js/application.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
   var game = new Game('0000202000000000');
 
-  debugger;
+  // debugger;
+
+
+  //put game on HTML
+  //bind keys
+   Mousetrap.bind('up', function() {
+      console.log('pressed Up');
+      //move game up
+      //update DOM
+
+   });
+
+   Mousetrap.bind('down', function() {
+      console.log('pressed down');
+      //move game down
+      //update DOM
+   });
+
+   Mousetrap.bind('left', function() {
+      console.log('pressed left');
+      //move game left
+      //update DOM
+   });
+
+   Mousetrap.bind('right', function() {
+      console.log('pressed right');
+      //move game right
+      //update DOM
+   });
 });
